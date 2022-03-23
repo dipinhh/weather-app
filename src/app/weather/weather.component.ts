aa8dce1f25c8999bffe577034221a11e589f1945
Dipin <dipinhemachandran@gmail.com>
2022-03-23 05:11:34 -0700

    added firebase

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { WeatherService } from './weather.service';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -9,16 +9,30 @@ import { WeatherService } from './weather.service';
 })
 export class WeatherComponent implements OnInit {
   cityName = new FormControl();
-  constructor(private weatherService: WeatherService) { }
+  searchHistory: any[] = [];
+  searchStatus: boolean = false;
+  constructor(private weatherService: WeatherService, private firestore: AngularFirestore) { 
+  }
 
   ngOnInit(): void {
   }
 
   getWeatherDetails() {
-    this.weatherService.getWeatherData(this.cityName.value).subscribe(res => {
-    });
+    this.weatherService.getWeatherData(this.cityName.value).subscribe((res: any) => { 
+      this.firestore.collection('searchData').add(res);
+       });
   }
   onSearchClick() {
+    this.searchStatus = false;
 this.getWeatherDetails();
+  }
+  
+  viewSearchHistory() {
+    this.searchStatus = true;
+   this.firestore.collection('searchData').valueChanges().subscribe(res => {
+    console.log(res, 'resres');
+    this.searchHistory = res;
+   });
+   
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDP6Q4RLvtymYluMiQAElMXi7DekrLwROo",
+    authDomain: "weather-app-fcc56.firebaseapp.com",
+    databaseURL: "https://weather-app-fcc56-default-rtdb.firebaseio.com",
+    projectId: "weather-app-fcc56",
+    storageBucket: "weather-app-fcc56.appspot.com",
+    messagingSenderId: "2784401011",
+    appId: "1:2784401011:web:ee69bed2e668aed69e3c06",
+    measurementId: "G-41Z9RG59SP"
+  }
 };
 
 /*
