879eb2ee6a9ffbb4801d7bb50f37e20c58ee1f7b
Dipin <dipinhemachandran@gmail.com>
2022-03-24 01:55:24 -0700

    added firebase confog to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDP6Q4RLvtymYluMiQAElMXi7DekrLwROo",
+    authDomain: "weather-app-fcc56.firebaseapp.com",
+    databaseURL: "https://weather-app-fcc56-default-rtdb.firebaseio.com",
+    projectId: "weather-app-fcc56",
+    storageBucket: "weather-app-fcc56.appspot.com",
+    messagingSenderId: "2784401011",
+    appId: "1:2784401011:web:ee69bed2e668aed69e3c06",
+    measurementId: "G-41Z9RG59SP"
+  }
 };
